de0e4e4d42febb0d831ca9ca7138e5a20d6d432c
Keith Rochester <keith.rochester@unit92.com>
2023-07-26 14:59:59 +0100

    First release of MP

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Simple-Ping
-Simple Ping SCOM Management Pack
+# Simple Ping SCOM Management Pack
+Monitoring is performed by the "Simple Ping Monitoring Resource Pool" which by default contains all management servers. Monitors the ping status matches the expected value either up or down.
+
+Configuration is performed using two agent tasks. 
+
+* Add Simple Ping
+* Remove Simple Ping
+
+
+# Adding Monitoring 
+
+>Navigate to Simple Ping > Resource Pool.
+![Resource Pool](<Screencaps/Resource Pool.png>)
+
+>Use the Add Simple Ping task overriding the task properties to configuring monitoring.
+![Overrides](Screencaps/Override.png)
+
+# Removing Monitoring
+
+>Navigate to Simple Ping > Targets. Select the target to remove and use the Remove Simple Ping task.
+![Remove](<Screencaps/Remove Target.png>)
---
+++ Simple Ping/Remove-SimplePing.ps1
@@ -0,0 +1,48 @@
+﻿[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$True)]
+	[string]$DisplayName,
+	[Parameter(Mandatory = $True)]
+	[String]$Address,
+	[Parameter(Mandatory = $True)]
+    [ValidateSet("UP","DOWN")]
+	[String]$ExpectedStatus,    
+	[Parameter(Mandatory = $false)]
+	[String]$ManagementServer = '.'
+
+)
+
+<#
+$DisplayName = "Testing"
+$Address = '127.0.0.1'
+$ExpectedStatus = "UP"
+$MatchCount = 1
+$FrequencySeconds = 300
+$ManagementServer = '.'
+#>
+
+Add-PSSnapin Microsoft.EnterpriseManagement.OperationsManager.Client
+New-SCOMManagementGroupConnection -ComputerName $ManagementServer
+$MG = Get-SCOMManagementGroup
+
+
+$EntityClass = Get-SCOMClass -Name System.Entity
+$PingTargetClass = Get-SCOMClass -Name Simple.Ping.Target
+
+$Connector = Get-SCOMConnector -DisplayName "Simple Ping Connector"
+if(!$Connector){
+    Add-SCOMConnector -Name "Simple Ping Connector" -DisplayName "Simple Ping Connector" -Description "Connector used to submit discovery data"
+    $Connector = Get-SCOMConnector -DisplayName "Simple Ping Connector"
+    }
+
+
+$DiscoveryData = New-Object Microsoft.EnterpriseManagement.ConnectorFramework.IncrementalDiscoveryData
+
+$PingTarget = New-Object Microsoft.EnterpriseManagement.Common.CreatableEnterpriseManagementObject($mg,$PingTargetClass)
+
+$PingTarget[$PingTargetClass,"Address"].Value = $Address
+$PingTarget[$PingTargetClass,"ExpectedStatus"].Value = $ExpectedStatus
+$PingTarget[$EntityClass,"DisplayName"].Value = $DisplayName
+
+$DiscoveryData.Remove($PingTarget)
+$DiscoveryData.Commit($Connector)
